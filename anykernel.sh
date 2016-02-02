23647d4021fa38155a9ed0aa1ff19e6b4519dc70
Meninblack007 <sanyam.53jain@gmail.com>
2016-02-02 03:41:27 -0500

    fix anykernel2
    Signed-off-by: Meninblack007 <sanyam.53jain@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -1,11 +1,11 @@
-# AnyKernel2 Ramdisk Mod Script
+# AnyKernel 2.0 Ramdisk Mod Script 
 # osm0sis @ xda-developers
 
 ## AnyKernel setup
 # EDIFY properties
-kernel.string=Dominator
 do.devicecheck=1
-do.initd=1
+do.system=0
+do.initd=0
 do.modules=1
 do.cleanup=1
 device.name1=YUREKA
@@ -15,7 +15,7 @@ device.name4=
 device.name5=
 
 # shell variables
-block=/dev/block/bootdevice/by-name/boot;
+block=/dev/block/platform/7824900.sdhci/by-name/boot;
 
 ## end setup
 
@@ -28,25 +28,20 @@ patch=/tmp/anykernel/patch;
 
 chmod -R 755 $bin;
 mkdir -p $ramdisk $split_img;
+cd $ramdisk;
 
-OUTFD=/proc/self/fd/$1;
-ui_print() { echo -e "ui_print $1\nui_print" > $OUTFD; }
+OUTFD=`ps | grep -v "grep" | grep -oE "update(.*)" | cut -d" " -f3`;
+ui_print() { echo "ui_print $1" >&$OUTFD; echo "ui_print" >&$OUTFD; }
 
 # dump boot and extract ramdisk
 dump_boot() {
   dd if=$block of=/tmp/anykernel/boot.img;
   $bin/unpackbootimg -i /tmp/anykernel/boot.img -o $split_img;
   if [ $? != 0 ]; then
-    ui_print " "; ui_print "Dumping/splitting image failed. Aborting..."; exit 1;
+    ui_print " "; ui_print "Dumping/unpacking image failed. Aborting...";
+    echo 1 > /tmp/anykernel/exitcode; exit;
   fi;
-  mv -f $ramdisk /tmp/anykernel/rdtmp;
-  mkdir -p $ramdisk;
-  cd $ramdisk;
   gunzip -c $split_img/boot.img-ramdisk.gz | cpio -i;
-  if [ $? != 0 -o -z "$(ls $ramdisk)" ]; then
-    ui_print " "; ui_print "Unpacking ramdisk failed. Aborting..."; exit 1;
-  fi;
-  cp -af /tmp/anykernel/rdtmp/* $ramdisk;
 }
 
 # repack ramdisk then build and write image
@@ -79,14 +74,10 @@ write_boot() {
   fi;
   cd $ramdisk;
   find . | cpio -H newc -o | gzip > /tmp/anykernel/ramdisk-new.cpio.gz;
-  if [ $? != 0 ]; then
-    ui_print " "; ui_print "Repacking ramdisk failed. Aborting..."; exit 1;
-  fi;
   $bin/mkbootimg --kernel $kernel --ramdisk /tmp/anykernel/ramdisk-new.cpio.gz $second --cmdline "$cmdline" --board "$board" --base $base --pagesize $pagesize --kernel_offset $kerneloff --ramdisk_offset $ramdiskoff $secondoff --tags_offset $tagsoff $dtb --output /tmp/anykernel/boot-new.img;
-  if [ $? != 0 ]; then
-    ui_print " "; ui_print "Repacking image failed. Aborting..."; exit 1;
-  elif [ `wc -c < /tmp/anykernel/boot-new.img` -gt `wc -c < /tmp/anykernel/boot.img` ]; then
-    ui_print " "; ui_print "New image larger than boot partition. Aborting..."; exit 1;
+  if [ $? != 0 -o `wc -c < /tmp/anykernel/boot-new.img` -gt `wc -c < /tmp/anykernel/boot.img` ]; then
+    ui_print " "; ui_print "Repacking Boot.img failed. Aborting operation...";
+    echo 1 > /tmp/anykernel/exitcode; exit;
   fi;
   dd if=/tmp/anykernel/boot-new.img of=$block;
 }
@@ -101,7 +92,7 @@ replace_string() {
   fi;
 }
 
-# insert_line <file> <if search string> <before|after> <line match string> <inserted line>
+# insert_line <file> <if search string> <before/after> <line match string> <inserted line>
 insert_line() {
   if [ -z "$(grep "$2" $1)" ]; then
     case $3 in
@@ -109,7 +100,7 @@ insert_line() {
       after) offset=1;;
     esac;
     line=$((`grep -n "$4" $1 | cut -d: -f1` + offset));
-    sed -i "${line}s;^;${5}\n;" $1;
+    sed -i "${line}s;^;${5};" $1;
   fi;
 }
 
@@ -136,19 +127,6 @@ prepend_file() {
   fi;
 }
 
-# insert_file <file> <if search string> <before|after> <line match string> <patch file>
-insert_file() {
-  if [ -z "$(grep "$2" $1)" ]; then
-    case $3 in
-      before) offset=0;;
-      after) offset=1;;
-    esac;
-    line=$((`grep -n "$4" $1 | cut -d: -f1` + offset));
-    sed -i "${line}s;^;\n;" $1;
-    sed -i "$((line - 1))r $patch/$5" $1;
-  fi;
-}
-
 # append_file <file> <if search string> <patch file>
 append_file() {
   if [ -z "$(grep "$2" $1)" ]; then
@@ -160,24 +138,8 @@ append_file() {
 
 # replace_file <file> <permissions> <patch file>
 replace_file() {
-  cp -pf $patch/$3 $1;
+  cp -fp $patch/$3 $1;
   chmod $2 $1;
-}
-
-# patch_fstab <fstab file> <mount match name> <fs match type> <block|mount|fstype|options|flags> <if search string> <replacement string>
-patch_fstab() {
-  entry=$(grep "$2" $1 | grep "$3");
-  if [ -z "$(echo "$entry" | grep "$5")" ]; then
-    case $4 in
-      block) part=$(echo "$entry" | awk '{ print $1 }');;
-      mount) part=$(echo "$entry" | awk '{ print $2 }');;
-      fstype) part=$(echo "$entry" | awk '{ print $3 }');;
-      options) part=$(echo "$entry" | awk '{ print $4 }');;
-      flags) part=$(echo "$entry" | awk '{ print $5 }');;
-    esac;
-    newentry=${entry//$part/$6};
-    sed -i "s;${entry};${newentry};" $1;
-  fi;
 }
 
 ## end methods
@@ -185,29 +147,23 @@ patch_fstab() {
 ## AnyKernel permissions
 # set permissions for included files
 chmod -R 755 $ramdisk
+chmod 750 $ramdisk/sbin/busybox;
 
 ## AnyKernel install
 dump_boot;
 
+# begin ramdisk changes
+
 # insert initd scripts
 
 # insert patches
-#cp -fp $patch/* /system/etc/
-#chmod 644 /system/etc/thermal-engine.conf
 
-# begin ramdisk changes
-
-#Add custom loader script
-found=$(find init.rc -type f | xargs grep -oh "import /init.dominator.rc");
-if [ "$found" != 'import /init.dominator.rc' ]; then
-	#append the new lines for this option at the bottom
-        echo "" >> init.rc
-	echo "import /init.dominator.rc" >> init.rc
-fi
+# adb secure
+replace_string default.prop "ro.adb.secure=0" "ro.adb.secure=1" "ro.adb.secure=0";
+replace_string default.prop "ro.secure=0" "ro.secure=1" "ro.secure=0";
 
 # end ramdisk changes
 
 write_boot;
 
-## end install
-
+## end install 
